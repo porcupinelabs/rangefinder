187824d57f4a46f8c4b25902d6ec19ed4e5de52b
porcupinelabs <info@porcupineelectronics.com>
2018-02-11 15:19:29 -0600

    Simple Arduino sample for LR4

---
+++ LR4 Sample Apps/Arduino/Simple/ArduinoSimple.cpp
@@ -0,0 +1,30 @@
+char measureBuf[6]; // enough space for five digits plus a zero byte
+int offset;
+void setup()
+{
+    Serial.begin(15200);
+    while (!Serial); // Wait untilSerial is ready (for Leonardo)
+    Serial1.begin(9600);
+    while (!Serial1);
+    Serial1.print("g"); // tell the LR4 to go
+    offset = 0;
+}
+
+void loop()
+{
+    if (Serial1.available()) {
+        char ch = Serial1.read();
+
+        if (ch == '\r') {
+            measureBuf[offset] = '\0'; // measureBuf now contains a string like "12345"
+            Serial.println(measureBuf); // do something with measureBuf
+            offset = 0;
+        }
+
+        if (ch == '\n')
+            offset = 0; // nothing to do, set offset to zero just to be safe
+
+        if (offset < 5)
+            measureBuf[offset++] = ch;
+    }
+}
